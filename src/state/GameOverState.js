027f0bfd25d62f74b5421b605940199e4a7e42f4
CurruptedMonk <dmitriy.petrov.1993@yandex.ru>
2023-03-13 20:14:02 +0300

    change game over text

--- src/state/GameOverState.js
+++ src/state/GameOverState.js
@@ -18,7 +18,7 @@ export default class {
         this.#game.gameLayer.context.fillStyle = "black";
         this.#game.gameLayer.context.textAlign = "center";
         this.#game.gameLayer.context.fillText("Game Over", this.#game.gameLayer.width /2, this.#game.gameLayer.height / 2.5);
-        this.#game.gameLayer.context.fillText("Press R to restart", this.#game.gameLayer.width /2, this.#game.gameLayer.height / 2);
+        this.#game.gameLayer.context.fillText("Press Spacebar to restart", this.#game.gameLayer.width /2, this.#game.gameLayer.height / 2);
         this.#game.gameLayer.context.fillText("or Esc to return to the menu", this.#game.gameLayer.width /2, this.#game.gameLayer.height / 1.8);
     }
 
